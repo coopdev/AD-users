3bbee92f4e9831c2cedffbfbeff428cd7ea2bb68
Joseph Workman <joeworkmn@gmail.com>
2013-01-04 00:33:42 -1000

    add html5 shim for IE8 and lower, add bootstrap-responsive.css

--- www/layout/header.php
+++ www/layout/header.php
@@ -4,8 +4,26 @@
 <head> 
    <script src="js/jquery.js" ?>"></script>
    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> 
+   <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
    <script src="bootstrap/js/bootstrap.min.js" ?>"></script>
+
+   <!-- This style is used to fix the problem where the page content is going into the header -->
+   <style type="text/css">
+      body {
+         padding-top: 60px;
+         padding-bottom: 40px;
+      }
+
+   </style>
+
+   <!-- IE 8 and lower do not support HTML5, so below IF
+        statement is necessary. -->
+
+   <!--[if lt IE 9]>
+       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+   <![endif]-->
 </head>
+
 
 <header class="navbar navbar-fixed-top navbar-inverse">
   <div class="navbar-inner">
@@ -27,4 +45,4 @@
       </nav>
     </div>
   </div>
-</header> <br /> <br /> <br /> <br />
+</header> 
--- www/ldif-file-list.php
+++ www/ldif-file-list.php
@@ -1,21 +1,23 @@
 <?php
 require_once('path-constants.php');
 require_once(HELPERS . '/funcs.php');
-
 require_once(APP_ROOT . '/www/layout/header.php');
 
 $jsonFile = file_get_contents(JSON_LIST);
 $ldifFiles = json_decode($jsonFile, true);
 $ldifFiles = array_reverse($ldifFiles);
 
-echo "<ul class='container'>";
-foreach ($ldifFiles as $fileName => $fileAttribs) {
-   echo "<li>";
+echo "<div class='container'>";
 
-   echo    "<a href='file-download.php?file=$fileName'> $fileName </a> | Status: " . $fileAttribs['status'];
+   echo "<div class='row'>"; // first row
+      echo "<ul class='span6'>";
+      foreach ($ldifFiles as $fileName => $fileAttribs) {
+         echo "<li>";
+            echo "<a href='file-download.php?file=$fileName'> $fileName </a> | Status: " . $fileAttribs['status'];
+         echo "</li>";
+      }
+      echo "</ul>";
 
-   echo "</li>";
-}
-echo "</ul>";
+echo "</div>";
 
 ?>
